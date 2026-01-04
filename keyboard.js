1402e99ea9e27ec6b770c7456248bb3116f300f7
Justin Emadem <kuajustemadem94@gmail.com>
2026-01-05 03:59:20 +0800

    make the light indicator work

--- keyboard.js
+++ keyboard.js
@@ -37,3 +37,33 @@ function updateButtonDisplay() {
     btn.textContent = shiftActive ? btn.dataset.shift : btn.dataset.normal;
   });
 }
+
+const capslockBtn = document.querySelector('.capslock');
+const capsLight = document.getElementById('caps-light');
+
+let capslockActive = false;
+
+capslockBtn.addEventListener('click', () => {
+    capslockActive = !capslockActive;
+    capslockBtn.classList.toggle('shift-active', capslockActive);
+    capsLight.classList.toggle('light-on', capslockActive);
+});
+
+const numlockBtn = document.querySelector('.numlk'); 
+const numLight = document.getElementById('num-light');
+let numlockActive = true; 
+
+numlockBtn.addEventListener('click', () => {
+    numlockActive = !numlockActive;
+    numLight.classList.toggle('light-on', numlockActive);
+});
+
+const winBtn = document.querySelectorAll('.btn-cwa.green')[1]; 
+const winLight = document.getElementById('win-light');
+
+winBtn.addEventListener('click', () => {
+    winLight.classList.add('light-on');
+    setTimeout(() => {
+        winLight.classList.remove('light-on');
+    }, 500);
+});
